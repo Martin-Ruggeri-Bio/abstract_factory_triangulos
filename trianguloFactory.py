7eff9cf2979037f101172c5ac8310c46f9aa1797
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2021-10-19 16:05:02 -0300

    fabrica abstracta de triangulos

---
+++ trianguloFactory.py
@@ -0,0 +1,14 @@
+from trianguloFactoryMetod import TrianguloFactoryMethod
+from equliatero import Equilatero
+from isosceles import Isosceles
+from escaleno import Escaleno
+
+class TrianguloFactory(TrianguloFactoryMethod):
+
+    def createTriangulo(self, ladoA, ladoB, ladoC):
+        if ((ladoA == ladoB) and (ladoA == ladoC)):
+            return Equilatero(ladoA,ladoB,ladoC)
+        elif ((ladoA != ladoB)and(ladoA != ladoC)and(ladoB != ladoC)):
+            return Escaleno(ladoA,ladoB,ladoC)
+        else:
+            return Isosceles(ladoA,ladoB,ladoC)
